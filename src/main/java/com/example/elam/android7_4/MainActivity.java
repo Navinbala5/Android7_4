047a3c55f4caed8960f7110beaeab4759feb8a6b
Navinbala5 <bnavin28@gmail.com>
2017-07-02 20:12:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/android7_4/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.elam.android7_4;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(android.view.View view) {
+        EditText username = (EditText) findViewById(R.id.editText);
+        EditText psw = (EditText) findViewById(R.id.editText2);
+        System.out.println("----------------" + username.getText().toString() + "------------psw--" + psw.getText().toString());
+        if("".equals(username.getText().toString()) || "".equals(psw.getText().toString())) {
+            Toast.makeText(getApplicationContext(),"ENTER THE DETAILS CORRECTLY",Toast.LENGTH_LONG).show();
+        } else {
+            String userNameData = String.valueOf(username.getText());
+            Intent intent = new Intent(MainActivity.this, AfterLoginActivity.class);
+            intent.putExtra("userName", userNameData);
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("type/plain");
+            this.startActivity(intent);
+        }
+    }
+}
+
